4af2bb832dab4f71c465d6b9617bdc168effe702
Dylan Singhabahu <dylansinghabahu360@gmail.com>
2023-08-13 10:21:01 +1000

    refine

--- testerstuff/tester/math_util.js
+++
@@ -1,9 +0,0 @@
-export const PI = 3.14519;
-
-export function getCircumference(radius) {
-    return 2 * PI * radius;
-}
-
-export function getArea(radius) {
-    return Math.pow(radius, 2) * PI;
-}
--- testerstuff/tester/something2.js
+++
@@ -1,3 +0,0 @@
-import {PI, getArea, getCircumference} from "./math_util.js";
-
-console.log(PI);
